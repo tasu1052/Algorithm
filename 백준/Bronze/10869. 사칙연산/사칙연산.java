2a4dfd6bb85bab64eb831a4bafe1e106e99e4b9f
tasu1052 <128462878+tasu1052@users.noreply.github.com>
2025-01-26 17:55:16 +0900

    [Bronze V] Title: 사칙연산, Time: 176 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] argc){
+        Scanner in= new Scanner(System.in);
+        int A = in.nextInt();
+        int B = in.nextInt();
+        in.close();
+        
+        System.out.println(A+B);
+        System.out.println(A-B);
+        System.out.println(A*B);
+        System.out.println(A/B);
+        System.out.println(A%B);
+    }
+}

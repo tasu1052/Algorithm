760be3954fb8772cd3dc0c8b08c4f7c0602d1638
tasu1052 <128462878+tasu1052@users.noreply.github.com>
2025-02-05 15:21:11 +0900

    [Bronze V] Title: 개수 세기, Time: 108 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        for(int i =0; i<N;i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+        int v = Integer.parseInt(br.readLine());
+        int count = 0;
+        
+        for(int i=0; i<N; i++) {
+            if(arr[i]==v)
+                count++;
+        }
+        System.out.print(count);
+        
+    }
+}

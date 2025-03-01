933fb584094674b2be7d7d1e82962e3bbefcf038
tasu1052 <128462878+tasu1052@users.noreply.github.com>
2025-03-01 17:19:54 +0900

    [Bronze IV] Title: 삼각형 외우기, Time: 104 ms, Memory: 14112 KB -BaekjoonHub

---
+++ 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int angle1 = Integer.parseInt(br.readLine());
+        int angle2 = Integer.parseInt(br.readLine());
+        int angle3 = Integer.parseInt(br.readLine());
+        int sum=angle1 + angle2 + angle3;
+        
+        if(sum!=180)
+            System.out.print("Error");
+        
+        if(angle1==60 && angle2==60 && angle3==60)
+            System.out.print("Equilateral");
+        else if((angle1==angle2 || angle2==angle3 || angle1==angle3) && sum==180)
+            System.out.print("Isosceles");
+        else if((angle1!=angle2 && angle2!=angle3 && angle1!=angle3) && sum==180)
+            System.out.print("Scalene");
+        
+    }
+}

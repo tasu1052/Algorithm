e4014e0c0def4a55b7e4b7f9811e2e9165a03f51
tasu1052 <128462878+tasu1052@users.noreply.github.com>
2025-03-14 15:10:15 +0900

    [Silver V] Title: 영화감독 숌, Time: 112 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -1,24 +1,83 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+public class Main{
+    public static void main(String[] args) throws IOException{
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
         int N = Integer.parseInt(br.readLine());
         
-        int count=0;
-        int index=666;
-        while(true){
-            String S = Integer.toString(index);
-            if(S.contains("666")){
-                count++;
+        if(N>1){
+            int count=1;
+            int prev_digit=0;
+            int num=0;
+            
+            while(true){
+                if((prev_digit%10000)/10==666 && prev_digit%10!=6){
+                    for(int i=0; i<1000;i++){
+                        if(count==N){
+                            System.out.print(prev_digit*1000+num);
+                            return;
+                        }
+                        num++;
+                        count++;
+                    }
+                    prev_digit++;
+                }
+                
+                else if(prev_digit%1000==666){
+                    num=0;
+                    for(int i=0;i<1000;i++){
+                        if(count==N){
+                            System.out.print(prev_digit*1000+num);
+                            return;
+                        }
+                        count++;
+                        num++;
+                    }
+                    prev_digit++;
+                }
+                
+                else if(prev_digit%100==66){
+                    num=600;
+                    for(int i=0; i<100; i++){
+                        if(count==N){
+                            System.out.print(prev_digit*1000+num);
+                            return;
+                        }
+                        count++;
+                        num++;
+                    }
+                    prev_digit++;
+                }
+                
+                else if(prev_digit%10==6){
+                    num=660;
+                    for(int i=0; i<10; i++){
+                        if(count==N){
+                            System.out.print(prev_digit*1000+num);
+                            return;
+                        }
+                        num++;
+                        count++;
+                    }
+                    prev_digit++;
+                }
+                
+                else{
+                    num=666;
+                    if(count==N){
+                        System.out.print(prev_digit*1000+num);
+                        return;
+                    }
+                    count++;
+                    prev_digit++;
+                }
             }
-            if(count==N){
-                System.out.print(index);
-                break;
-            }
-            index++;
-        }  
+        }
+        
+        else
+            System.out.println(666);
     }
 }

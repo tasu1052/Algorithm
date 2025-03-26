585d83a6258962405917a4bf2a050ffc0b470620
tasu1052 <128462878+tasu1052@users.noreply.github.com>
2025-03-26 23:21:37 +0900

    [Bronze III] Title: 팩토리얼, Time: 100 ms, Memory: 14132 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        if(N == 0){
+            System.out.print(1);
+            return;
+        }
+        int sum=1;
+        for(int i=1; i<=N; i++){
+            sum *= i;
+        }
+        System.out.print(sum);
+    }
+}

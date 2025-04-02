b2b23f7f48908bb4b54445517289c99e9889bfea
tasu1052 <128462878+tasu1052@users.noreply.github.com>
2025-04-02 22:04:46 +0900

    [Silver II] Title: 베르트랑 공준, Time: 144 ms, Memory: 14688 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
@@ -2,21 +2,26 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    public static int size = 246913;
+    public static boolean[] Prime = new boolean[size];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
         StringBuilder sb = new StringBuilder();
+        get_Prime();
         while(true){
             int n = Integer.parseInt(br.readLine());
             if(n==0) break;
-            sb.append(getPrimeNumber(n)).append('\n');
+            int count = 0;
+            
+            for(int i=n+1; i<=2*n; i++)
+                if(!Prime[i]) count++;
+            sb.append(count).append('\n');
         }
         System.out.print(sb);
     }
     
-    public static int getPrimeNumber(int num){
-        int size = 246913;
-        boolean[] Prime = new boolean[size];
+    public static void get_Prime(){
         Prime[0] = Prime[1] = true;
         for(int i=2; i*i < size; i++){
             if(!Prime[i]){
@@ -24,11 +29,5 @@ public class Main {
                     Prime[j] = true;
             }
         }
-        int count=0;
-        for(int i=num+1; i<=2*num; i++){
-            if(!Prime[i])
-                count++;
-        }
-        return count;
     }
 }

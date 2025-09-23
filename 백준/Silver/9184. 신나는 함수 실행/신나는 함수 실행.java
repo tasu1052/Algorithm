3dd1c002031023327656256d3ab4584a32c51b09
tasu1052 <128462878+tasu1052@users.noreply.github.com>
2025-09-23 14:12:49 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 260 ms, Memory: 25948 KB -BaekjoonHub

---
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int[][][] arr = new int[21][21][21];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            if (a == -1 && b == -1 && c == -1) break;
+
+            sb.append("w(").append(a).append(", ").append(b).append(", ").append(c).append(") = ")
+              .append(w(a, b, c)).append('\n');
+        }
+
+        System.out.print(sb);
+    }
+
+    public static int w(int a, int b, int c) {
+        if (a <= 0 || b <= 0 || c <= 0) return 1;
+        if (a > 20 || b > 20 || c > 20) return w(20, 20, 20);
+
+        if (arr[a][b][c] != 0) return arr[a][b][c];
+
+        if (a < b && b < c) {
+            arr[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
+        } else {
+            arr[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) +
+                           w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
+        }
+
+        return arr[a][b][c];
+    }
+}

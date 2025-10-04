e5a9446f5d7d85a74b0377e592dc9f55c8da8e36
tasu1052 <128462878+tasu1052@users.noreply.github.com>
2025-10-04 16:41:06 +0900

    [D1] Title: 더블더블, Time: 73 ms, Memory: 23,168 KB -BaekjoonHub

--- SWEA/D1/2019. 더블더블/더블더블.java
+++ SWEA/D1/2019. 더블더블/더블더블.java
@@ -1,19 +1,15 @@
 import java.io.*;
-import java.util.*;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
-        int sum = 1;
 
-        sb.append(sum).append(" ");
-        
-        for(int i=0; i<N; i++){
-            sum = sum * 2; 
-            sb.append(sum).append(" ");
+        for (int i = 0; i <= N; i++) {
+            sb.append((int)Math.pow(2, i)).append(" ");
         }
+
         System.out.print(sb);
     }
 }

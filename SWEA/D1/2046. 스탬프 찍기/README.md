2bfe6e98718b8ac911ab7a2e1ea5b241abf0148d
tasu1052 <128462878+tasu1052@users.noreply.github.com>
2025-10-04 16:35:08 +0900

    [D1] Title: 스탬프 찍기, Time: 72 ms, Memory: 22,912 KB -BaekjoonHub

---
+++ SWEA/D1/2046. 스탬프 찍기/README.md
@@ -0,0 +1,15 @@
+# [D1] 스탬프 찍기 - 2046 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5QKdT6AyYDFAUq) 
+
+### 성능 요약
+
+메모리: 22,912 KB, 시간: 72 ms, 코드길이: 414 Bytes
+
+### 제출 일자
+
+2025-10-04 16:35
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D1/2046. 스탬프 찍기/스탬프 찍기.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0; i<N; i++){
+            sb.append('#');
+        }
+        System.out.print(sb);
+    }
+}

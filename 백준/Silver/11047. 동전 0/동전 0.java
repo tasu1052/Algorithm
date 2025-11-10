267fae7102bf7eb7b777d51b59be4135986a17c7
tasu1052 <128462878+tasu1052@users.noreply.github.com>
2025-11-10 18:55:46 +0900

    [Silver IV] Title: 동전 0, Time: 100 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        
+        st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] coin = new int[N];
+
+        for(int i=0; i<N; i++) coin[i] = Integer.parseInt(br.readLine());
+
+        int count = 0;
+        for(int i=N-1; i>=0; i--){
+            count += K / coin[i];
+            K = K % coin[i];
+        }
+        System.out.print(count);
+    }
+}

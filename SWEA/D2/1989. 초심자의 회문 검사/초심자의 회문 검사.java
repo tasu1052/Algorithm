72fa47f3f1b7a224933e6bb8460a9783fd54d02a
tasu1052 <128462878+tasu1052@users.noreply.github.com>
2025-11-20 18:26:03 +0900

    [D2] Title: 초심자의 회문 검사, Time: 84 ms, Memory: 24,832 KB -BaekjoonHub

--- SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.java
+++ SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.java
@@ -2,29 +2,26 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
+    static int[] num = new int[10];
+    static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
-        
-        for(int i=1; i<=T; i++){
-            String str = br.readLine();
-            String[] arr = str.split("");
+   
+        for(int t=1; t<=T; t++){
+            char[] c = br.readLine().toCharArray();
 
-            boolean alltrue = true;
-            for(int j=0; j<Math.ceil(arr.length/2.0); j++){
-                if(!arr[j].equals(arr[arr.length-1-j])){
-                    alltrue =false;
+            boolean check = true;
+            for(int i=0, j=c.length-1; i<c.length/2; i++, j--){
+                if(c[i] != c[j]){
+                    check = false;
                     break;
                 }
             }
-            if(alltrue)
-                sb.append("#").append(i).append(" ").append(1).append('\n');
-            else
-                sb.append("#").append(i).append(" ").append(0).append('\n');
-            
+            if(check) sb.append("#").append(t).append(" ").append(1).append('\n');
+            else sb.append("#").append(t).append(" ").append(0).append('\n');
         }
         System.out.print(sb);
-    }
+    } 
 }

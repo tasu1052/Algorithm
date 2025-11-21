d4d58b8c0bb2f6566d929723ebcef542cffadc32
tasu1052 <128462878+tasu1052@users.noreply.github.com>
2025-11-21 15:40:33 +0900

    [D2] Title: 조교의 성적 매기기, Time: 135 ms, Memory: 31,616 KB -BaekjoonHub

--- SWEA/D2/1983. 조교의 성적 매기기/조교의 성적 매기기.java
+++ SWEA/D2/1983. 조교의 성적 매기기/조교의 성적 매기기.java
@@ -2,40 +2,40 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-    static int[][] arr = new int[9][9];
+    static int[] num = new int[10];
+    static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
-
-        String[] grade = new String[]{"A+", "A0", "A-", "B+", "B0", "B-", "C+", "C0", "C-", "D0"};
+   
+        String[] grade = new String[] {"A+", "A0", "A-", "B+","B0",
+        		"B-", "C+", "C0", "C-", "D0"};
         
         for(int t=1; t<=T; t++){
             st = new StringTokenizer(br.readLine());
             int N = Integer.parseInt(st.nextToken());
             int K = Integer.parseInt(st.nextToken());
-            Double[] score = new Double[N];
+            Double[] studentScore = new Double[N];
             
-            for(int i=0; i<N; i++){
-                st = new StringTokenizer(br.readLine());
-                double total = Integer.parseInt(st.nextToken()) * 0.35 +
-                    Integer.parseInt(st.nextToken()) * 0.45 +
-                    Integer.parseInt(st.nextToken()) * 0.2;
-                
-                score[i] = total;
+            for(int i=0; i<N; i++) {
+            	st = new StringTokenizer(br.readLine());
+            	double total = 0.35 * Integer.parseInt(st.nextToken()) +
+            			0.45 * Integer.parseInt(st.nextToken()) + 
+            					0.2 * Integer.parseInt(st.nextToken());
+            	studentScore[i] = total;
             }
-            double targetScore = score[K-1];
+            double targetScore = studentScore[K-1];
+            Arrays.sort(studentScore, (a,b)-> Double.compare(b,a));
             
-            Arrays.sort(score, Collections.reverseOrder());
-
-            int idx = 0;
-            for(int i=0; i<N; i++){
-                if(i != 0 && i%(N/10) == 0) idx++;
-                if(score[i] == targetScore)
-                    sb.append("#").append(t).append(" ").append(grade[idx]).append('\n');
+            for(int i=0; i<N; i++) {
+            	if(studentScore[i] == targetScore) {
+            		sb.append("#").append(t).append(" ")
+            		.append(grade[(int)(i/(N/10))]).append('\n');
+            		break;
+            	}
             }
-            
         }
         System.out.print(sb);
     }

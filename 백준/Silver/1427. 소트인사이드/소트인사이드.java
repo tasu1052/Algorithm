13a3ee3e3056dff27b650b893f7403b94fbd7e9a
tasu1052 <128462878+tasu1052@users.noreply.github.com>
2025-11-23 17:29:09 +0900

    [Silver V] Title: 소트인사이드, Time: 104 ms, Memory: 14400 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,28 +1,20 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main {
+class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
         StringBuilder sb = new StringBuilder();
         
-        String N = br.readLine();
-        
-        int[] cnt = new int[10];
-        
-        for(int i=0; i<N.length(); i++){
-            char c = N.charAt(i);
-            cnt[c - '0']++;
-        }
-        
-        for(int i=9; i>=0; i--){
-            while(cnt[i]>0){
-                sb.append(i);
-                cnt[i]--;
-            }
-        }
-        
+        String s = br.readLine();
+        Character[] arr = new Character[s.length()];
+        for(int i=0; i<s.length(); i++) arr[i] = s.charAt(i);
+
+        Arrays.sort(arr, (a, b) -> b - a);
+
+
+        for(Character c : arr) sb.append(c);
         System.out.print(sb);
-        
     }
 }

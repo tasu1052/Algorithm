d2019e069baa43909614f6ef211a8cca158d52eb
tasu1052 <128462878+tasu1052@users.noreply.github.com>
2025-12-02 18:12:46 +0900

    [Gold IV] Title: N-Queen, Time: 5916 ms, Memory: 14500 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -2,47 +2,39 @@ import java.io.*;
 import java.util.*;
 
 public class Main{
-    static boolean[][] arr;
+    static int[] arr;
     static int N;
-    static int count;
+    static int count = 0;
     
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
         N = Integer.parseInt(br.readLine());
 
-        arr = new boolean[N][N];
+        arr = new int[N];
         
         nqueen(0);
 
         System.out.print(count);
         
     }
-    static boolean isSafe(int row, int col){
+    static boolean check(int row){
         for(int i=0; i<row; i++){
-            if(arr[i][col]) return false;
-        }
-
-        for(int i=row-1, j=col-1; i>=0&&j>=0; i--, j--){
-            if(arr[i][j]) return false;
-        }
-
-        for(int i=row-1, j=col+1; i>=0 && j < N; i--, j++){
-            if(arr[i][j]) return false;
+            if(arr[i] == arr[row]) return false;
+            if(Math.abs(row - i) == Math.abs(arr[i] - arr[row])) return false;
         }
         return true;
     }
-    static void nqueen(int depth){
-        if(depth == N){
+    static void nqueen(int row){
+        if(row == N){
             count++;
             return;
         }
 
         for(int i=0; i<N; i++){
-            if(isSafe(depth, i)){
-                arr[depth][i] = true;
-                nqueen(depth+1);
-                arr[depth][i] = false;
+            arr[row] = i;
+            if(check(row)){
+                nqueen(row + 1);
             }
         }
     }

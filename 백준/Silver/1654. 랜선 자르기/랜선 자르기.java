451b243e9b01c24cc69fdfa6969ba12356ae4ea0
tasu1052 <128462878+tasu1052@users.noreply.github.com>
2025-12-11 15:11:53 +0900

    [Silver II] Title: 랜선 자르기, Time: 152 ms, Memory: 16508 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -4,34 +4,40 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
         StringBuilder sb = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        st = new StringTokenizer(br.readLine());
         int K = Integer.parseInt(st.nextToken());
         int N = Integer.parseInt(st.nextToken());
-        int[] line = new int[K];
-        
-        long max = 0;
-        for (int i = 0; i < K; i++) {
-            line[i] = Integer.parseInt(br.readLine());
-            if (line[i] > max) max = line[i];
+
+        int[] arr = new int[K];
+
+        int max = 0;
+        for(int i=0; i<K; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+            max = Math.max(max, arr[i]);
         }
-        
-        long left = 1;
-        long right = max;
+
+        long start = 1;
+        long end = max;
         long result = 0;
-        while(left<=right){
-            long mid = (left + right) / 2;
-            long total = 0;
+
+        while(start <= end){
+            long mid = (start + end) / 2;
+            long cnt = 0;
+
             for(int i=0; i<K; i++){
-                total += line[i]/mid;
+                cnt += arr[i] / mid;
             }
-            if(total >= N){
+
+            if(cnt >= N){
                 result = mid;
-                left = mid + 1;
-            } 
-            else
-                right = mid - 1;
+                start = mid + 1;
+            }
+            else end = mid - 1;
         }
+        
         System.out.print(result);
     }
 }

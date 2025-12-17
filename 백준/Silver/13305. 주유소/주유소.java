b03e7df848e450cb62c3894a07eb6394497c4e1a
tasu1052 <128462878+tasu1052@users.noreply.github.com>
2025-12-17 15:52:01 +0900

    [Silver III] Title: 주유소, Time: 308 ms, Memory: 30592 KB, Score: 58 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -11,39 +11,20 @@ public class Main {
 
         int[] road = new int[N-1];
         st = new StringTokenizer(br.readLine());
-        long maxLength = 0;
-        for(int i=0; i<N-1; i++){
-            road[i] = Integer.parseInt(st.nextToken());
-            maxLength += road[i];
-        }
+        for(int i=0; i<N-1; i++) road[i] = Integer.parseInt(st.nextToken());
         
         int[] city = new int[N];
         st = new StringTokenizer(br.readLine());
         for(int i=0; i<N; i++) city[i] = Integer.parseInt(st.nextToken());
 
         long cost = 0;
-        long length = 0;
+        int minPrice = city[0];
 
-        int cityIdx = 0;
-        int roadIdx = 0;
-        while(cityIdx != N-1){
-            if(city[cityIdx] > city[cityIdx + 1]){
-                cost += city[cityIdx] * road[roadIdx];
-                cityIdx++;
-                roadIdx++;
-            }
-            else{
-                int tmpLength = 0;
-                int curCityIdx = cityIdx;
-                cityIdx++;
-                while(city[curCityIdx] < city[cityIdx]){
-                    tmpLength += road[roadIdx++];
-                    cityIdx++;
-                }
-                tmpLength += road[roadIdx++];
-                cost += city[curCityIdx] * tmpLength;
-            }
+        for(int i=0; i<N-1; i++){
+            minPrice = Math.min(minPrice, city[i]);
+            cost += minPrice * road[i];
         }
+        
         System.out.print(cost);
     }
 }

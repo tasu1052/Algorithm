90c3268351a9ae4cc6e8e41851deddd43759a5b9
tasu1052 <128462878+tasu1052@users.noreply.github.com>
2025-12-17 16:22:42 +0900

    [Silver III] Title: 1로 만들기, Time: 120 ms, Memory: 18212 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -4,15 +4,20 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
         int N = Integer.parseInt(br.readLine());
-        int[] dp = new int[N+1];
-        
-        dp[1] = 0;
+
+        int[] D = new int[N+1];
+
+        D[1] = 0;
+
         for(int i=2; i<=N; i++){
-           dp[i] = dp[i-1] + 1;
-            if(i%2==0) dp[i] = Math.min(dp[i], dp[i/2] + 1);
-            if(i%3==0) dp[i] = Math.min(dp[i], dp[i/3] + 1);
+            D[i] = D[i-1] + 1;
+            if(i % 2 == 0) D[i] = Math.min(D[i], D[i/2] + 1);
+            if(i % 3 == 0) D[i] = Math.min(D[i], D[i/3] + 1);
         }
-        System.out.print(dp[N]);
+        System.out.print(D[N]);
     }
 }

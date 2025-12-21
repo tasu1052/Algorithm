5f8b42457258812d80890d434220564c93d9442f
tasu1052 <128462878+tasu1052@users.noreply.github.com>
2025-12-21 21:55:14 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 104 ms, Memory: 14348 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        long[] D = new long[N+1];
+
+        D[1] = 1;
+        if(N>=2) D[2] = 3;
+
+        for(int i=3; i<=N; i++){
+            D[i] = (D[i-1] + (D[i-2] * 2)) % 10007;
+        }
+        
+        System.out.print(D[N]);
+    }
+}

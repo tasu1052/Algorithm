9b0569f502b4103e6b716b0faefefa4a736b1af0
tasu1052 <128462878+tasu1052@users.noreply.github.com>
2025-12-22 17:04:15 +0900

    [Silver II] Title: 연속합, Time: 232 ms, Memory: 23804 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -4,23 +4,27 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
 
-        for(int i=0; i<N; i++){
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N+1];
+        st = new StringTokenizer(br.readLine());
+        for(int i=1; i<=N; i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
+
+        int[] D = new int[N+1];
+
+        D[1] = arr[1];
+
+        int ans = arr[1];
         
-        int[] dp = new int[N];
-        dp[0] = arr[0];
-        int max = dp[0];
-
-        for(int i=1; i<N; i++){
-            dp[i] = Math.max(arr[i], dp[i-1] + arr[i]);
-            max = Math.max(max, dp[i]);
+        for(int i=2; i<=N; i++){
+            D[i] = Math.max(arr[i], D[i-1] + arr[i]);
+            ans = Math.max(ans, D[i]);
         }
-
-        System.out.print(max);
+        System.out.print(ans);
     }
 }

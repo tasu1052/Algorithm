b85103509c6c5fec93db8db643a9f28668dbb6d7
tasu1052 <128462878+tasu1052@users.noreply.github.com>
2026-01-19 21:17:50 +0900

    [Gold V] Title: AC, Time: 808 ms, Memory: 100884 KB -BaekjoonHub

---
+++ 백준/Gold/5430. AC/AC.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int tc = 1; tc <= T; tc++) {
+            String com = br.readLine();
+            int n = Integer.parseInt(br.readLine());
+            String s = br.readLine();
+
+            Deque<Integer> q = new ArrayDeque<>();
+
+            if (n > 0) {
+                s = s.substring(1, s.length() - 1); // [ ]
+                String[] arr = s.split(",");
+                for (String x : arr) {
+                    q.add(Integer.parseInt(x));
+                }
+            }
+
+            boolean reverse = false;
+            boolean isError = false;
+
+            for (int i = 0; i < com.length(); i++) {
+                char c = com.charAt(i);
+
+                if (c == 'R') {
+                    reverse = !reverse;
+                } else if (c == 'D') {
+                    if (q.isEmpty()) {
+                        isError = true;
+                        break;
+                    }
+                    if (reverse) q.pollLast();
+                    else q.pollFirst();
+                }
+            }
+
+            if (isError) {
+                sb.append("error\n");
+                continue;
+            }
+
+            sb.append("[");
+            while (q.size() > 1) {
+                if (reverse) sb.append(q.pollLast()).append(",");
+                else sb.append(q.pollFirst()).append(",");
+            }
+            if (!q.isEmpty()) sb.append(q.poll());
+            sb.append("]\n");
+        }
+
+        System.out.print(sb);
+    }
+}

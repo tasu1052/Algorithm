542bf006bb3fe8576b02986ce9c9dbaf5134134f
tasu1052 <128462878+tasu1052@users.noreply.github.com>
2026-01-29 10:51:19 +0900

    [D3] Title: [S/W 문제해결 기본] 7일차 - 암호생성기, Time: 101 ms, Memory: 27,392 KB -BaekjoonHub

--- SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
@@ -1,41 +1,41 @@
 import java.io.*;
 import java.util.*;
-    
 public class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = 10;
-        StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-            
-        for(int t=1; t<=T; t++){
-            int tc = Integer.parseInt(br.readLine());
-            Deque<Integer> deque = new LinkedList<>();
-            st = new StringTokenizer(br.readLine());
-
-            for(int i=0; i<8; i++){
-                deque.offerLast(Integer.parseInt(st.nextToken()));
-            }
-            
-            Label:
-            while(true){
-                for(int i=1; i<=5; i++){
-                    int a = deque.pollFirst() - i;
-
-                    if(a<=0){
-                        a = 0;
-                        deque.offerLast(a);
-                        break Label;
-                    }
-                    deque.offerLast(a);
-                }
-            }
-            sb.append("#").append(tc).append(" ");
-            for(int i=0; i<8; i++){
-                sb.append(deque.pollFirst()).append(" ");
-            }
-            sb.append('\n');
+        StringBuilder sb = new StringBuilder();
+ 
+        //int T = Integer.parseInt(br.readLine());
+         
+        for (int t = 1; t <= 10; t++) {
+        	int T = Integer.parseInt(br.readLine());
+        	Deque<Integer> q = new ArrayDeque<>();
+        	
+        	st = new StringTokenizer(br.readLine());
+        	for(int i=0; i<8; i++) q.add(Integer.parseInt(st.nextToken()));
+        	
+        	int num = 1;
+        	while(true) {
+        		int tmp = q.pollFirst() - num;
+        		if(tmp <= 0) {
+        			tmp = 0;
+        			q.addLast(tmp);
+        			break;
+        		}
+        		q.addLast(tmp);
+        		
+        		num++;
+        		
+        		if(num > 5) num = 1;
+        	}
+        	
+        	sb.append("#").append(t).append(" ");
+        	while(!q.isEmpty()) sb.append(q.pollFirst() + " ");
+        	sb.append('\n');
         }
         System.out.print(sb);
-    }
+	}
+
 }

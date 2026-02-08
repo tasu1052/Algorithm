b9071b82fd5ddf9c662c32e4be80fa553313412d
tasu1052 <128462878+tasu1052@users.noreply.github.com>
2026-02-08 16:59:02 +0900

    [Silver III] Title: 괄호 끼워넣기, Time: 104 ms, Memory: 14408 KB -BaekjoonHub

---
+++ 백준/Silver/11899. 괄호 끼워넣기/괄호 끼워넣기.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        //StringTokenizer st;
+
+        String s = br.readLine();
+
+        Stack<Character> st = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                st.push(c);
+            } else if(c == ')'){
+                if(st.isEmpty()) st.push(c);
+                else if(st.peek() == '(') st.pop();
+                else st.push(c);
+            }
+        }
+
+        System.out.print(st.size());
+    }
+}

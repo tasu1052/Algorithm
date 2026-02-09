503bcff1c0165ea830e15bdce4b7e28062bd7b0a
tasu1052 <128462878+tasu1052@users.noreply.github.com>
2026-02-09 16:43:26 +0900

    [Silver IV] Title: 비트가 넘쳐흘러, Time: 220 ms, Memory: 20060 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/17419. 비트가 넘쳐흘러/비트가 넘쳐흘러.java
+++ 백준/Silver/17419. 비트가 넘쳐흘러/비트가 넘쳐흘러.java
@@ -2,23 +2,15 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String s = br.readLine().trim();
 
-		int N = Integer.parseInt(br.readLine());
-		String s = br.readLine();
-		
-		int K = Integer.parseInt(s, 2);
-		
-		int calCnt = 0;
-		
-		while(K != 0) {
-			//연산
-			K = K-(K&((~K) + 1));
-			calCnt++;
-		}
-		System.out.println(calCnt);
-	}
+        int cnt = 0;
+        for (int i = 0; i < N; i++) {
+            if (s.charAt(i) == '1') cnt++;
+        }
+        System.out.println(cnt);
+    }
 }

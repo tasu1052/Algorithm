97a922ecfcec4439d545dddf4a9679c4eed01fac
tasu1052 <128462878+tasu1052@users.noreply.github.com>
2026-02-11 10:46:58 +0900

    [Gold V] Title: 회의실 배정, Time: 456 ms, Memory: 42108 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.java
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -2,46 +2,42 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        StringBuilder sb = new StringBuilder();
+        //StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
 
-        PriorityQueue<Meeting> q = new PriorityQueue<>((a, b) -> {
-            if(a.end == b.end) return a.start - b.start;
-            return a.end - b.end;
+        PriorityQueue<Conference> q = new PriorityQueue<>((a, b) -> {
+        	if(a.end == b.end) return Integer.compare(a.start, b.start);
+        	return Integer.compare(a.end, b.end);
         });
-
-        for(int i=0; i<N; i++){
-            st = new StringTokenizer(br.readLine());
-            
-            int s = Integer.parseInt(st.nextToken());
-            int e = Integer.parseInt(st.nextToken());
-
-            q.add(new Meeting(s, e));
-        }
-
-        int cnt = 1;
-        int startTime = q.poll().end;
         
-        while(!q.isEmpty()){
-            Meeting cur = q.poll();
-            if(startTime <= cur.start){
-                startTime = cur.end;
-                cnt++;
-            }
+        for(int i=0; i<N; i++) {
+        	st = new StringTokenizer(br.readLine());
+        	int s = Integer.parseInt(st.nextToken());
+        	int e = Integer.parseInt(st.nextToken());
+        	
+        	q.add(new Conference(s, e));
         }
-        System.out.print(cnt);
+        int cnt = 0;
+        while(!q.isEmpty()) {
+        	Conference now = q.poll();
+        	cnt++;
+        	
+        	while(!q.isEmpty() && now.end > q.peek().start) q.poll();
+        }
+        
+        System.out.println(cnt);
     }
 }
-class Meeting{
-    int start;
-    int end;
-
-    public Meeting(int start, int end){
-        this.start = start;
-        this.end = end;
-    }
+class Conference{
+	int start;
+	int end;
+	
+	Conference(int start, int end){
+		this.start = start;
+		this.end = end;
+	}
 }

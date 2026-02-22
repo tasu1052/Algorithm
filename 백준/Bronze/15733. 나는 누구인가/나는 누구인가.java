f0357287946b489f6448095f4b7954abc27a1962
tasu1052 <128462878+tasu1052@users.noreply.github.com>
2026-02-22 14:16:57 +0900

    [Bronze V] Title: 나는 누구인가, Time: 100 ms, Memory: 14076 KB -BaekjoonHub

---
+++ 백준/Bronze/15733. 나는 누구인가/나는 누구인가.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        System.out.print("I'm Sexy");
+    }
+}

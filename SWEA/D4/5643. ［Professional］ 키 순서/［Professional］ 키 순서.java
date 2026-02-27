3965fef8396f3b7283a4e75e9e6b99f4c3d8bb9b
tasu1052 <128462878+tasu1052@users.noreply.github.com>
2026-02-27 16:26:44 +0900

    [D4] Title: [Professional] 키 순서, Time: 1,785 ms, Memory: 99,620 KB -BaekjoonHub

--- SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -1,67 +1,74 @@
 import java.io.*;
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class Solution {
+	static int N, M;
+	static ArrayList<Integer>[] A, B;
+	static int ans;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
         StringBuilder sb = new StringBuilder();
-        
+
         int T = Integer.parseInt(br.readLine());
-
+        
         for (int tc = 1; tc <= T; tc++) {
-            int N = Integer.parseInt(br.readLine());
-            int M = Integer.parseInt(br.readLine());
-
-            List<Integer>[] g = new ArrayList[N + 1];
-            List<Integer>[] rg = new ArrayList[N + 1];
-            for (int i = 1; i <= N; i++) {
-                g[i] = new ArrayList<>();
-                rg[i] = new ArrayList<>();
-            }
-
-            for (int i = 0; i < M; i++) {
-                StringTokenizer st = new StringTokenizer(br.readLine());
-                int a = Integer.parseInt(st.nextToken());
-                int b = Integer.parseInt(st.nextToken());
-                g[a].add(b);
-                rg[b].add(a);
-            }
-
-            int ans = 0;
-            for(int i=1; i<=N; i++) {
-            	int bigger = bfs(i, g, N);
-            	int smaller = bfs(i, rg, N);
+            N = Integer.parseInt(br.readLine());
+            M = Integer.parseInt(br.readLine());
+            
+            A = new ArrayList[N+1];
+            for(int i=0; i<=N; i++) A[i] = new ArrayList<>();
+            
+            B = new ArrayList[N+1];
+            for(int i=0; i<=N; i++) B[i] = new ArrayList<>();
+            
+            ans = 0;
+            
+            for(int i=0; i<M; i++) {
+            	st = new StringTokenizer(br.readLine());
+            	int s = Integer.parseInt(st.nextToken());
+            	int e = Integer.parseInt(st.nextToken());
             	
-            	if(bigger + smaller == N-1) ans++;
+            	A[s].add(e);
+            	B[e].add(s);
             }
             
-
+            for(int i=1; i<=N; i++) {
+            	boolean[] v = new boolean[N+1];
+            	
+            	bfs(i, v, A);
+            	bfs(i, v, B);
+            	
+            	boolean check = true;
+            	for(int j=1; j<=N; j++) {
+            		if(!v[j]) {
+            			check = false;
+            			break;
+            		}
+            	}
+            	
+            	if(check) ans++;
+            }
             sb.append('#').append(tc).append(' ').append(ans).append('\n');
         }
 
         System.out.print(sb);
     }
-    
-    static int bfs(int start, List<Integer>[] g, int N) {
-    	boolean[] visited = new boolean[N+1];
-    	    	
+    static void bfs(int start, boolean[] v, ArrayList<Integer>[] list) {
     	ArrayDeque<Integer> q = new ArrayDeque<>();
     	q.add(start);
-    	visited[start] = true;
+    	v[start] = true;
     	
-    	int cnt = 0;
     	while(!q.isEmpty()) {
     		int now = q.poll();
     		
-    		for(int next : g[now]) {
-    			if(!visited[next]) {
-    				visited[next] = true;
-    				q.add(next);
-    				cnt++;
-    			}
+    		for(int next : list[now]) {
+	    			if(!v[next]) {
+	    			v[next] = true;
+	    			q.add(next);
+	    		}
     		}
     	}
-    	
-    	return cnt;
     }
 }

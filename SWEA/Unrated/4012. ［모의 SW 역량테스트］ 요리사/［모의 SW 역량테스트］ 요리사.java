35715175af6d0bc776a851b7048fe5fd45e348b0
tasu1052 <128462878+tasu1052@users.noreply.github.com>
2026-03-01 16:48:49 +0900

    [Unrated] Title: [모의 SW 역량테스트] 요리사, Time: 221 ms, Memory: 28,688 KB -BaekjoonHub

--- SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
+++ SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
@@ -1,67 +1,64 @@
 import java.io.*;
 import java.util.*;
 
-class Solution {
+public class Solution {
     static int N;
-    static int[][] arr;
-    static int A;
-    static int B;
+    static int[] dx = {-1, 1, 1, -1};
+    static int[] dy = {1, 1, -1, -1};
+    static int[][] map;
+    static boolean[] picked;
     static int ans;
-    static boolean[] pick;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
-        
+
         int T = Integer.parseInt(br.readLine());
 
         for(int tc=1; tc<=T; tc++){
             N = Integer.parseInt(br.readLine());
 
-            arr = new int[N][N];
-            pick = new boolean[N];
-            
+            map = new int[N][N];
+            picked = new boolean[N];
+            ans = Integer.MAX_VALUE;
+
             for(int i=0; i<N; i++){
                 st = new StringTokenizer(br.readLine());
                 for(int j=0; j<N; j++){
-                    arr[i][j] = Integer.parseInt(st.nextToken());
+                    map[i][j] = Integer.parseInt(st.nextToken());
                 }
-            } //배열 초기화
+            }
 
-            ans = Integer.MAX_VALUE;
-            pick[0] = true;
-            dfs(1, 1);
-
+            dfs(0, 0);
+            
             sb.append("#").append(tc).append(" ").append(ans).append('\n');
         }
+
         System.out.print(sb);
     }
+    static void dfs(int start, int cnt){
+        if(cnt == N/2){
+            int firstFood = 0, secondFood = 0;
 
-    static void dfs(int idx, int chosen){
-        if(chosen == N/2){
-            cal();
-            return;
-        }
-
-        if(idx == N) return;
-
-        pick[idx] = true;
-        dfs(idx + 1, chosen + 1);
-
-        pick[idx] = false;
-        dfs(idx + 1, chosen);
-    }
-    static void cal(){
-        int aSum = 0, bSum = 0;
-
-        for(int i=0; i<N-1; i++){
-            for(int j=i+1; j<N; j++){
-                int s = arr[i][j] + arr[j][i];
-                if(pick[i] && pick[j]) aSum += s;
-                else if(!pick[i] && !pick[j]) bSum += s;
+            for(int i=0; i<N-1; i++){
+                for(int j=i+1; j<N; j++){
+                    if(picked[i] && picked[j]){
+                        firstFood += map[i][j] + map[j][i];
+                    }
+                    else if(!picked[i] && !picked[j]){
+                        secondFood += map[i][j] + map[j][i];
+                    }
+                }
             }
+
+            ans = Math.min(ans, Math.abs(firstFood - secondFood));
+            
         }
 
-        ans = Math.min(ans, Math.abs(aSum - bSum));
+        for(int i=start; i<N; i++){
+            picked[i] = true;
+            dfs(i + 1, cnt + 1);
+            picked[i] = false;
+        }
     }
 }

c770208c1222566fdddcabf642863c022eb17410
tasu1052 <128462878+tasu1052@users.noreply.github.com>
2026-03-10 14:38:30 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 690 ms, Memory: 95,512 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -2,89 +2,84 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-    static int[] parent;
-    static double E;
-    static long[] xs, ys;
-
-    public static void main(String[] args) throws Exception{
+	static int[] parent;
+	static double ans;
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
 
         int T = Integer.parseInt(br.readLine());
-
-        for(int tc = 1; tc <= T; tc++) {
-            int N = Integer.parseInt(br.readLine().trim());
-
-            xs = new long[N];
-            ys = new long[N];
-
-            // X 좌표 N개 (한 줄)
-            st = new StringTokenizer(br.readLine());
-            for(int i=0; i<N; i++) xs[i] = Long.parseLong(st.nextToken());
-
-            // Y 좌표 N개 (한 줄)
-            st = new StringTokenizer(br.readLine());
-            for(int i=0; i<N; i++) ys[i] = Long.parseLong(st.nextToken());
-
-            E = Double.parseDouble(br.readLine().trim());
-
-            ArrayList<Edge> edges = new ArrayList<>();
-
-            for(int i=0; i<N-1; i++){
-                for(int j=i+1; j<N; j++){
-                    long dx = xs[i] - xs[j];
-                    long dy = ys[i] - ys[j];
-                    double w = E * (dx*dx + dy*dy);
-                    edges.add(new Edge(i, j, w));
-                }
-            }
-
-            Collections.sort(edges);
-
-            parent = new int[N];
-            for(int i=0; i<N; i++) parent[i] = i;
-
-            double ans = 0.0;
+        
+        for (int tc = 1; tc <= T; tc++) {
+        	int N = Integer.parseInt(br.readLine());
+        	
+        	int[] x = new int[N];
+        	
+        	st = new StringTokenizer(br.readLine());
+        	for(int i=0; i<N; i++) x[i] = Integer.parseInt(st.nextToken());
+        	
+        	int[] y = new int[N];
+        	
+        	st = new StringTokenizer(br.readLine());
+        	for(int i=0; i<N; i++) y[i] = Integer.parseInt(st.nextToken());
+        	
+        	double E = Double.parseDouble(br.readLine());
+        	
+        	ArrayList<Island> arr = new ArrayList<>();
+        	for(int i=0; i<N-1; i++) {
+        		for(int j=i; j<N; j++) {
+        			double sum = E * (Math.pow(x[i] - x[j], 2) + Math.pow(y[i] - y[j], 2));
+        			arr.add(new Island(i, j, sum));
+        		}
+        	}
+        	
+        	parent = new int[N];
+        	for(int i=0; i<N; i++) parent[i] = i;
+        	
+            Collections.sort(arr);
+            
             int edgeCnt = 0;
             int idx = 0;
-
-            while(edgeCnt < N-1){
-                Edge e = edges.get(idx++);
-                if(find(e.u) != find(e.v)){
-                    union(e.u, e.v);
-                    ans += e.w;
-                    edgeCnt++;
-                }
+            ans = 0.0;
+            
+            while(edgeCnt != N-1) {
+            	Island tmp = arr.get(idx++);
+            	
+            	if(find(tmp.from) != find(tmp.to)) {
+            		union(tmp.from, tmp.to);
+            		edgeCnt++;
+            		ans += tmp.weight;
+            	}
             }
-
-            sb.append('#').append(tc).append(' ')
-                    .append(Math.round(ans)).append('\n');
+            
+            sb.append("#").append(tc).append(" ").append(Math.round(ans)).append('\n');
         }
 
         System.out.print(sb);
     }
-
-    static void union(int a, int b){
-        a = find(a);
-        b = find(b);
-        if(a != b) parent[b] = a;
+    static void union(int a, int b) {
+    	a = find(a);
+    	b = find(b);
+    	if(a != b) parent[b] = a;
     }
-
-    static int find(int x){
-        if(parent[x] == x) return x;
-        return parent[x] = find(parent[x]);
+    static int find(int x) {
+    	if(x == parent[x]) return x;
+    	return parent[x] = find(parent[x]);
     }
-
-    static class Edge implements Comparable<Edge>{
-        int u, v;
-        double w;
-        Edge(int u, int v, double w){
-            this.u = u; this.v = v; this.w = w;
-        }
-        @Override
-        public int compareTo(Edge o){
-            return Double.compare(this.w, o.w);
-        }
-    }
+}
+class Island implements Comparable<Island>{
+	int from, to;
+	double weight;
+	
+	Island(int from, int to, double weight){
+		this.from = from;
+		this.to = to;
+		this.weight = weight;
+	}
+	
+	@Override
+	public int compareTo(Island o) {
+		return Double.compare(weight, o.weight);
+	}
 }

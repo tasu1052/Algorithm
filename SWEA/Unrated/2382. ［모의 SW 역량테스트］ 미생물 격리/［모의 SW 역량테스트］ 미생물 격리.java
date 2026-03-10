a50a3742bb1a5bb1cfbb9d486bbc32f93f20f1e7
tasu1052 <128462878+tasu1052@users.noreply.github.com>
2026-03-10 15:30:55 +0900

    [Unrated] Title: [모의 SW 역량테스트] 미생물 격리, Time: 429 ms, Memory: 115,904 KB -BaekjoonHub

--- SWEA/Unrated/2382. ［모의 SW 역량테스트］ 미생물 격리/［모의 SW 역량테스트］ 미생물 격리.java
+++ SWEA/Unrated/2382. ［모의 SW 역량테스트］ 미생물 격리/［모의 SW 역량테스트］ 미생물 격리.java
@@ -2,96 +2,94 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-    static int N, M, K;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
-
-    static ArrayDeque<Micro> q;
-    static int ans;
-
+	static int N, M, K;
+	static int[] dx = {-1, 1, 0, 0};
+	static int[] dy = {0, 0, -1, 1};
+	static int ans;
+	static micro[][] map;
+	static ArrayDeque<micro> q;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
         StringBuilder sb = new StringBuilder();
 
-        int T = Integer.parseInt(br.readLine().trim());
+        int T = Integer.parseInt(br.readLine());
+        
         for (int tc = 1; tc <= T; tc++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+        	st = new StringTokenizer(br.readLine());
+        	
             N = Integer.parseInt(st.nextToken());
             M = Integer.parseInt(st.nextToken());
             K = Integer.parseInt(st.nextToken());
-
-            q = new ArrayDeque<>(K);
-
-            for (int i = 0; i < K; i++) {
-                st = new StringTokenizer(br.readLine());
-                int r = Integer.parseInt(st.nextToken());
-                int c = Integer.parseInt(st.nextToken());
-                int num = Integer.parseInt(st.nextToken());
-                int dir = Integer.parseInt(st.nextToken()) - 1;
-
-                q.add(new Micro(r, c, num, dir));
+            
+            q = new ArrayDeque<>();
+            
+            for(int i=0; i<K; i++) {
+            	st = new StringTokenizer(br.readLine());
+            	int x = Integer.parseInt(st.nextToken());
+            	int y = Integer.parseInt(st.nextToken());
+            	int num = Integer.parseInt(st.nextToken());
+            	int dir = Integer.parseInt(st.nextToken()) - 1;
+            	
+            	q.add(new micro(x, y, num, dir));
             }
-
-            simulate();
-
-            sb.append('#').append(tc).append(' ').append(ans).append('\n');
+            
+            ans = 0;
+            
+            for(int i=0; i<M; i++) bfs();
+            
+            while(!q.isEmpty()) ans += q.poll().num;
+            
+            sb.append("#").append(tc).append(" ").append(ans).append('\n');
         }
 
         System.out.print(sb);
     }
-
-    static void simulate() {
-        for (int t = 0; t < M; t++) {
-            Micro[][] next = new Micro[N][N];
-            int[][] max = new int[N][N]; 
-
-            int size = q.size();
-            for (int s = 0; s < size; s++) {
-                Micro cur = q.poll();
-
-                int nx = cur.r + dx[cur.dir];
-                int ny = cur.c + dy[cur.dir];
-
-                int num = cur.num;
-                int dir = cur.dir;
-
-                if (nx == 0 || nx == N - 1 || ny == 0 || ny == N - 1) {
-                    num /= 2;
-                    dir = dir ^ 1;
-                    if (num == 0) continue;
-                }
-
-                if (next[nx][ny] == null) {
-                    next[nx][ny] = new Micro(nx, ny, num, dir);
-                    max[nx][ny] = num;
-                } else {
-                    next[nx][ny].num += num;
-
-                    if (num > max[nx][ny]) {
-                        max[nx][ny] = num;
-                        next[nx][ny].dir = dir;
-                    }
-                }
-            }
-
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    if (next[i][j] != null) q.add(next[i][j]);
-                }
-            }
-        }
-
-        ans = 0;
-        for (Micro m : q) ans += m.num;
+    static void bfs() {
+    	micro[][] map = new micro[N][N];
+    	int[][] max = new int[N][N];
+    	
+    	while(!q.isEmpty()) {
+    		micro tmp = q.poll();
+    		int x = tmp.x, y = tmp.y, num = tmp.num, dir = tmp.dir;
+    		
+    		int nx = x + dx[dir], ny = y + dy[dir];
+    		
+    		if(nx == 0 || nx == N-1 || ny == 0 || ny == N-1) {
+    			num /= 2;
+    			dir = dir ^ 1;
+    			if(num == 0) continue;
+    		}
+    		
+    		if(map[nx][ny] != null) {
+    			map[nx][ny].num += num;
+    			if(max[nx][ny] < num) {
+    				max[nx][ny] = num;
+    				map[nx][ny].dir = dir;
+    			}
+    		}
+    		else if(map[nx][ny] == null) {
+    			map[nx][ny] = new micro(nx, ny, num, dir);
+    			max[nx][ny] = num;
+    		}
+    	}
+    	
+    	for(int i=0; i<N; i++) {
+    		for(int j=0; j<N; j++) {
+    			if(map[i][j] != null) {
+    				q.add(map[i][j]);
+    			}
+    		}
+    	}
     }
-
-    static class Micro {
-        int r, c, num, dir;
-        Micro(int r, int c, int num, int dir) {
-            this.r = r;
-            this.c = c;
-            this.num = num;
-            this.dir = dir;
-        }
-    }
+}
+class micro{
+	int x, y, num, dir;
+	
+	micro(int x, int y, int num, int dir){
+		this.x = x;
+		this.y = y;
+		this.num = num;
+		this.dir = dir;
+	}
 }

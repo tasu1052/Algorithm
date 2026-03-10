3bc5acb2d911e7dc58aeec5630bfa68b95ce71fc
tasu1052 <128462878+tasu1052@users.noreply.github.com>
2026-03-10 14:15:31 +0900

    [D5] Title: [S/W 문제해결 응용] 3일차 - 최적 경로, Time: 223 ms, Memory: 26,624 KB -BaekjoonHub

--- SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.java
+++ SWEA/D5/1247. ［S／W 문제해결 응용］ 3일차 － 최적 경로/［S／W 문제해결 응용］ 3일차 － 최적 경로.java
@@ -2,74 +2,63 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-    static int N, comX, comY, homeX, homeY;
-    static Customer[] arr;
-    static boolean[] v;
-    static int ans;
-    // static int[] dx = {0, 1, 0, -1};
-    // static int[] dy = {1, 0, -1, 0};
-    
+	static int N;
+	static int homeX, homeY;
+	static int[] cx, cy;
+	static int ans;
+	static boolean[] v;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
 
         int T = Integer.parseInt(br.readLine());
-
-        for(int tc=1; tc<=T; tc++){
-            N = Integer.parseInt(br.readLine());
-
-            arr = new Customer[N];
-            v = new boolean[N];
-            ans = Integer.MAX_VALUE;
+        
+        for (int tc = 1; tc <= T; tc++) {
+        	N = Integer.parseInt(br.readLine());
+        	
+        	cx = new int[N];
+        	cy = new int[N];
+        	v = new boolean[N];
+        	
+        	st = new StringTokenizer(br.readLine());
+        	
+            int comX = Integer.parseInt(st.nextToken());
+            int comY = Integer.parseInt(st.nextToken());
             
-            st = new StringTokenizer(br.readLine());
-
-            comX = Integer.parseInt(st.nextToken());
-            comY = Integer.parseInt(st.nextToken());
-
             homeX = Integer.parseInt(st.nextToken());
             homeY = Integer.parseInt(st.nextToken());
-
-            for(int i=0; i<N; i++){
-                int x = Integer.parseInt(st.nextToken());
-                int y = Integer.parseInt(st.nextToken());
-
-                arr[i] = new Customer(x, y);
+            
+            for(int i=0; i<N; i++) {
+            	cx[i] = Integer.parseInt(st.nextToken());
+            	cy[i] = Integer.parseInt(st.nextToken());
             }
 
-            dfs(0, comX, comY, comX, comY, 0);
-
+            ans = Integer.MAX_VALUE;
+            
+            dfs(comX, comY, 0, 0);
+            
             sb.append("#").append(tc).append(" ").append(ans).append('\n');
         }
 
         System.out.print(sb);
     }
-    static void dfs(int idx, int x, int y, int lastX, int lastY, int dist){
-        if(idx == N){
-            ans = Math.min(ans, dist + cal(homeX, homeY, x, y));
-            return;
-        }
-
-        if(dist >= ans) return;
-
-        for(int i=0; i<N; i++){
-            if(v[i]) continue;
-            v[i] = true;
-            dfs(idx + 1, arr[i].x, arr[i].y, x, y, 
-                dist + cal(arr[i].x, arr[i].y, x, y));
-            v[i] = false;
-        }
+    static void dfs(int x, int y, int sum, int cnt) {
+    	if(cnt == N) {
+    		ans = Math.min(ans, sum + calDist(x, y, homeX, homeY));
+    		return;
+    	}
+    	
+    	if(sum >= ans) return; // 거리가 이미 ans보다 크면 어처피 안되니까 가지치기
+    	
+    	for(int i=0; i<N; i++) {
+    		if(v[i]) continue;
+    		v[i] = true;
+    		dfs(cx[i], cy[i], sum + calDist(x, y, cx[i], cy[i]), cnt + 1);
+    		v[i] = false;
+    	}
     }
-    static int cal(int x, int y, int lastX, int lastY){
-        return Math.abs(x - lastX) + Math.abs(y - lastY);
-    }
-}
-class Customer{
-    int x, y;
-
-    Customer(int x, int y){
-        this.x = x;
-        this.y = y;
+    static int calDist(int lastX, int lastY, int x, int y) {
+    	return Math.abs(lastX - x) + Math.abs(lastY - y);
     }
 }

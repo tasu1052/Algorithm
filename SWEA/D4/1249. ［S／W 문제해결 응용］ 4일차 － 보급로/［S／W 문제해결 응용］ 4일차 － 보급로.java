82b4b10c68e3b83c6c2002be8f20b82cdbaf6ea1
tasu1052 <128462878+tasu1052@users.noreply.github.com>
2026-03-13 10:40:57 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 132 ms, Memory: 36,512 KB -BaekjoonHub

--- SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
@@ -5,7 +5,7 @@ public class Solution {
 	static int N;
 	static int[][] map;
 	static int[][] dist;
-	static int[] dx = {0, 1, 0, -1};
+	static int[] dx = {0, 1, 0, -1}; // 우, 하, 좌, 상
     static int[] dy = {1, 0, -1, 0};
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));

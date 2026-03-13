03b5a71729d29cfaf75f0bb309b61d6e1458f0f5
tasu1052 <128462878+tasu1052@users.noreply.github.com>
2026-03-13 10:21:05 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 132 ms, Memory: 36,512 KB -BaekjoonHub

--- SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
@@ -4,54 +4,53 @@ import java.util.*;
 public class Solution {
 	static int N;
 	static int[][] map;
-	static int[] dx = {0, 1, 0, -1};
-	static int[] dy = {1, 0, -1, 0};
 	static int[][] dist;
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        StringBuilder sb = new StringBuilder();
-
-        int T = Integer.parseInt(br.readLine());
-        
-        for (int tc = 1; tc <= T; tc++) {
-            N = Integer.parseInt(br.readLine());
-            map = new int[N][N];
-            dist = new int[N][N];
-            for(int i=0; i<N; i++) Arrays.fill(dist[i], Integer.MAX_VALUE);
-            
-            for(int i=0; i<N; i++) {
-            	String line = br.readLine();
-            	for(int j=0; j<N; j++) {
-            		map[i][j] = line.charAt(j) - '0';
-            	}
-            }
-            
-            ArrayDeque<int[]> q = new ArrayDeque<>();
-            q.add(new int[] {0, 0});
-            dist[0][0] = 0;
-            
-            while(!q.isEmpty()) {
-            	int[] now = q.poll();
-            	int x = now[0];
-            	int y = now[1];
-            	
-            	for(int k=0; k<4; k++) {
-            		int nx = x + dx[k];
-            		int ny = y + dy[k];
-            		
-            		if(nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
-            		
-            		if(dist[nx][ny] > dist[x][y] + map[nx][ny]) {
-            			dist[nx][ny] = dist[x][y] + map[nx][ny];
-            			q.add(new int[] {nx, ny});
-            		}
-            	}
-            }
-
-            sb.append('#').append(tc).append(' ').append(dist[N-1][N-1]).append('\n');
-        }
-
-        System.out.print(sb);
-    }
+	static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int tc=1; tc<=T; tc++) {
+			N = Integer.parseInt(br.readLine());
+			
+			map = new int[N][N];
+			dist = new int[N][N];
+			for(int i=0; i<N; i++) Arrays.fill(dist[i], Integer.MAX_VALUE);
+			
+			for(int i=0; i<N; i++) {
+				String line = br.readLine();
+				for(int j=0; j<N; j++) {
+					map[i][j] = line.charAt(j) - '0';
+				}
+			}
+			
+			ArrayDeque<int[]> q = new ArrayDeque<>();
+			dist[0][0] = 0;
+			q.add(new int[] {0, 0});
+			
+			while(!q.isEmpty()) {
+				int[] cur = q.poll();
+				int x = cur[0], y = cur[1];
+				
+				for(int k=0; k<4; k++) {
+					int nx = x + dx[k], ny = y + dy[k];
+					
+					if(nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
+					
+					if(dist[nx][ny] > dist[x][y] + map[nx][ny]) {
+						dist[nx][ny] = dist[x][y] + map[nx][ny];
+						q.add(new int[] {nx, ny});
+					}
+				}
+			}
+			
+			sb.append('#').append(tc).append(' ').append(dist[N-1][N-1]).append('\n');
+		}
+		
+		System.out.println(sb);
+	}
 }

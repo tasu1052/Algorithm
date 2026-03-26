37f1c45e0a86947667ed195fa37b8840658f449e
tasu1052 <128462878+tasu1052@users.noreply.github.com>
2026-03-26 14:18:16 +0900

    [Gold V] Title: 집합의 표현, Time: 692 ms, Memory: 55344 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -1,43 +1,44 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
-    static int[] arr;
-    public static void main(String[] args) throws Exception{
+public class Main {
+	static int N;
+	static int[] parent;
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
 
         st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-
-        arr = new int[n+1];
-        for(int i=0; i<n+1; i++) arr[i] = i;
-
-        for(int i=0; i<m; i++){
-            st = new StringTokenizer(br.readLine());
-            int com = Integer.parseInt(st.nextToken());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-
-            if(com == 0){
-                union(a, b);
-            }
-            else{
-                if(find(a) == find(b)) sb.append("YES\n");
-                else sb .append("NO\n");
-            }
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        
+        parent = new int[N+1];
+        for(int i=0; i<=N; i++) parent[i] = i;
+        
+        for(int i=0; i<M; i++) {
+        	st = new StringTokenizer(br.readLine());
+        	int com = Integer.parseInt(st.nextToken());
+        	int a = Integer.parseInt(st.nextToken());
+        	int b = Integer.parseInt(st.nextToken());
+        	
+        	if(com == 0) union(a, b);
+        	else {
+        		if(find(a) == find(b)) sb.append("YES").append('\n');
+        		else sb.append("NO").append('\n');
+        	}
         }
-        System.out.print(sb);
+        
+        System.out.println(sb);
     }
-    static void union(int a, int b){
-        a = find(a);
-        b = find(b);
-        if(a != b) arr[b] = a;
+    static void union(int a, int b) {
+    	a = find(a);
+    	b = find(b);
+    	
+    	if(a != b) parent[b] = a;
     }
-    static int find(int x){
-        if(arr[x] == x) return x;
-        return arr[x] = find(arr[x]);
+    static int find(int x) {
+    	if(x == parent[x]) return x;
+    	return parent[x] = find(parent[x]);
     }
 }

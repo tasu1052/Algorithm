c3563199315a4448dd4057decebc4654319c552b
tasu1052 <128462878+tasu1052@users.noreply.github.com>
2026-04-23 10:26:35 +0900

    [level 3] Title: 이중우선순위큐, Time: 99.70 ms, Memory: 21.2 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -2,25 +2,42 @@ import heapq
 def solution(operations):
     maxh = []
     minh = []
+    visited = [False] * len(operations)
+    
     for i in range(len(operations)):
         com, num = operations[i].split()
         num = int(num)
         
         if com == "I":
-            heapq.heappush(maxh, -num)
-            heapq.heappush(minh, num)
-        elif maxh and com == "D":
+            heapq.heappush(maxh, (-num, i))
+            heapq.heappush(minh, (num, i))
+            visited[i] = True
+            
+        else:
             if num == 1:
-                remove_value = -heapq.heappop(maxh)
-                minh.remove(remove_value)
-                heapq.heapify(minh)
-            else: #최소값 삭제
-                remove_value = -heapq.heappop(minh)
-                maxh.remove(remove_value)
-                heapq.heapify(maxh)
-             
-    if not maxh:
-        return [0, 0]
-    else:
-        return [-heapq.heappop(maxh), heapq.heappop(minh)]
+                while maxh and not visited[maxh[0][1]]:
+                    heapq.heappop(maxh)
                 
+                if maxh:
+                    visited[maxh[0][1]] = False
+                    heapq.heappop(maxh)
+                    
+            else:
+                while minh and not visited[minh[0][1]]:
+                    heapq.heappop(minh)
+                
+                if minh:
+                    visited[minh[0][1]] = False
+                    heapq.heappop(minh)
+                
+
+    while maxh and not visited[maxh[0][1]]:
+        heapq.heappop(maxh)
+
+    while minh and not visited[minh[0][1]]:
+        heapq.heappop(minh)
+
+    if not maxh or not minh:
+        return [0, 0]
+
+    return [-maxh[0][0], minh[0][0]]

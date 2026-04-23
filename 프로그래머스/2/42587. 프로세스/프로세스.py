84a2f04b27b791d3f76555aa201b2d9090dad216
tasu1052 <128462878+tasu1052@users.noreply.github.com>
2026-04-23 11:02:20 +0900

    [level 2] Title: 프로세스, Time: 3.33 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,21 +1,16 @@
 from collections import deque
 def solution(priorities, location):
-    q = deque(priorities)
-    idx = deque()
-    for i in range(len(priorities)):
-        idx.append(i)
+    q = deque(enumerate(priorities))
     
     order = 0
     while q:
-        x = q.popleft()
-        ix = idx.popleft()
+        idx, priority = q.popleft()
         
-        if q and x < max(q):
-            q.append(x)
-            idx.append(ix)
+        if q and priority < max(p for _, p in q):
+            q.append((idx, priority))
         else:
             order += 1
-            if ix == location:
+            if idx == location:
                 return order
                     
     

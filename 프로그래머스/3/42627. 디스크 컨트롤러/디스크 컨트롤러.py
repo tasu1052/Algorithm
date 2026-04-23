738c0264c0c7ec9aeb3383ff183755d4af3cd21c
tasu1052 <128462878+tasu1052@users.noreply.github.com>
2026-04-23 09:40:38 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.96 ms, Memory: 9.13 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,26 +1,27 @@
 import heapq
 def solution(jobs):
-    jobs.sort() #요청시간, 소요시간 기준으로 정렬
+    jobs.sort()
     
-    n = len(jobs) #작업 개수
-    total = 0 #전체 반환시간
-    done = 0 #완료한 작업 수
-    idx = 0 #아직 도착안한 다음 작업
-    time = 0 #현재 시각
-    heap = [] #소요시간, 요청시간 기준 정렬하는 우선순위 큐
+    n = len(jobs)
+    waitq = []
+    time = 0
+    idx = 0
+    done = 0
+    total = 0
+    totalResponseTime = []
     
     while done < n:
-        while idx < n and jobs[idx][0] <= time: #현재 시간보다 더 빨리 요청된 작업 전부 힙에 넣음
-            request, duration = jobs[idx]
-            heapq.heappush(heap, (duration, request))
+        while idx < n and jobs[idx][0] <= time:
+            heapq.heappush(waitq, (jobs[idx][1], jobs[idx][0]))
             idx += 1
             
-        if heap:
-            duration, request = heapq.heappop(heap)
+        if waitq:
+            duration, request = heapq.heappop(waitq)
             time += duration
-            total += time - request
+            total = time - request
+            totalResponseTime.append(total)
             done += 1
         else:
             time = jobs[idx][0]
-    return total // n
-    
+        
+    return sum(totalResponseTime) // len(totalResponseTime)

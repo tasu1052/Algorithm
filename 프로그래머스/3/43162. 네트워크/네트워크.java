b42b1187afe3d95580027bdaf5c555ee950db7b4
tasu1052 <128462878+tasu1052@users.noreply.github.com>
2026-07-20 17:37:34 +0900

    [level 3] Title: 네트워크, Time: 1.20 ms, Memory: 84.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,26 +1,41 @@
+import java.util.*;
+
 class Solution {
+    static ArrayList<Integer>[] list;
     static int ans;
     static boolean[] visited;
     public int solution(int n, int[][] computers) {
+        list = new ArrayList[n];
         visited = new boolean[n];
         ans = 0;
+        for(int i=0; i<n; i++) list[i] = new ArrayList<>();
         
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if(computers[i][j] == 1 && !visited[j]){
-                    visited[j] = true;
-                    dfs(j, n, computers);
-                    ans++;
+        for(int i=0; i<computers.length; i++){
+            for(int j=0; j<computers[0].length; j++){
+                if(i == j) continue;
+                
+                if(computers[i][j] == 1){
+                    list[i].add(j);
+                    list[j].add(i);
                 }
             }
         }
+        
+        for(int i=0; i<n; i++){
+            if(!visited[i]){
+                dfs(i);
+                ans++;
+            }
+        }
+        
         return ans;
     }
-    static void dfs(int idx, int n, int[][] computers){
-        for(int j=0; j<n; j++){
-            if(computers[idx][j] == 1 && !visited[j]){
-                visited[j] = true;
-                dfs(j, n, computers);
+    static void dfs(int now){
+        visited[now] = true;
+        
+        for(int next : list[now]){
+            if(!visited[next]){
+                dfs(next);
             }
         }
     }

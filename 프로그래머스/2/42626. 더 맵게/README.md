877a3937549ae3699283e954a992590b192a8046
tasu1052 <128462878+tasu1052@users.noreply.github.com>
2026-08-03 22:19:26 +0900

    [level 2] Title: 더 맵게, Time: 651.61 ms, Memory: 140 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/README.md
+++ 프로그래머스/2/42626. 더 맵게/README.md
@@ -1,10 +1,10 @@
 # [level 2] 더 맵게 - 42626 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626?language=java) 
 
 ### 성능 요약
 
-메모리: 69.5 MB, 시간: 1714.31 ms
+메모리: 140 MB, 시간: 651.61 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 18일 15:08:36
+2026년 08월 03일 22:19:20
 
 ### 문제 설명
 
--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,25 +1,28 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[] scoville, int K) {
-        PriorityQueue<Long> q = new PriorityQueue<>((a, b) -> {
-            return Long.compare(a, b);
-        });
+        //우선순위 큐 사용해서 모든 음식 넣기
+        //두개씩 꺼내서 계산하고 K 보다 낮으면 다시 넣기
+        //이때 처음 꺼낸 음식이 K보다 크면 break하고 섞은 횟수 return
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
         
-        for(int i=0; i<scoville.length; i++) q.add((long)scoville[i]);
-        
-        int answer = 0;
-        
-        while(!q.isEmpty() && q.peek() < K){
-            if(q.size() < 2) return -1;
-            
-            long first = q.poll();
-            long second = q.poll();
-            
-            long newFood = first + (second * 2);
-            
-            q.add(newFood);
-            answer++;
+        for(int i=0; i<scoville.length; i++){
+            pq.add(scoville[i]);
         }
-        return answer;
+        
+        int cnt = 0;
+        while(true){
+            if(pq.size() < 2 && pq.peek() < K) return -1;
+            
+            int first = pq.poll();
+            if(first >= K) return cnt;
+            int second = pq.poll();
+            
+            int newFood = first + (second * 2);
+            
+            cnt++;
+            pq.add(newFood);
+        }
     }
 }

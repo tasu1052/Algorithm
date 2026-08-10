1ffef3240e96946c2d979daf28460951fde3a15e
tasu1052 <128462878+tasu1052@users.noreply.github.com>
2026-08-10 10:51:44 +0900

    [level 2] Title: 삼각 달팽이, Time: 6.12 ms, Memory: 144 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -1,25 +1,25 @@
 class Solution {
     public int[] solution(int n) {
-        //1 = 1, 2 = 3, 3 = 6, 4 = 10, 5 = 15, 6 = 21
+        //어떤 방향으로 나아가야 하면 dx, dy 생각하기
+        //좌표 갱신 후 어떤 조건 되면 방향 바꾸는 시뮬레이션
         int[] dx = {1, 0, -1};
         int[] dy = {0, 1, -1};
         
         int[][] arr = new int[n][n];
-        int dir = 0;
         int num = 1;
+        int dir = 0;
         int x = 0, y = 0;
         for(int i=n; i>=1; i--){
             for(int j=0; j<i; j++){
-                arr[x][y] = num;
+                arr[x][y] = num++;
                 
                 x += dx[dir];
                 y += dy[dir];
-                
-                num++;
             }
             
             x -= dx[dir];
             y -= dy[dir];
+            
             dir = (dir + 1) % 3;
             
             x += dx[dir];
@@ -28,11 +28,10 @@ class Solution {
         
         int[] answer = new int[n * (n+1) / 2];
         int idx = 0;
+        
         for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if(arr[i][j] != 0){
-                    answer[idx++] = arr[i][j];
-                }
+            for(int j=0; j<=i; j++){
+                answer[idx++] = arr[i][j];
             }
         }
         return answer;

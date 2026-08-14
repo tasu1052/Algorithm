261c07e08017432fc1c8f1835cf3aaf5239615b9
tasu1052 <128462878+tasu1052@users.noreply.github.com>
2026-08-14 13:10:03 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.40 ms, Memory: 11.1 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,26 +1,17 @@
 from collections import deque
 dx = [0, 1, 0, -1]
 dy = [1, 0, -1, 0]
-INF = 10000
-n = 0
-m = 0
 def solution(maps):
-    global n, m
-    n = len(maps)
-    m = len(maps[0])
-    dist = [[INF] * m for _ in range(n)]
+    dist = [[-1] * len(maps[0]) for _ in range(len(maps))]
     
-    bfs(0, 0, maps, dist)
+    bfs(maps, dist)
     
-    if dist[n-1][m-1] == 10000:
-        return -1
-    return dist[n-1][m-1]
+    return dist[len(maps)-1][len(maps[0])-1]
 
-def bfs(i, j, maps, dist):
-    global dx, dy, INF, n, m
+def bfs(maps, dist):
     q = deque()
-    dist[i][j] = 1
-    q.append((i, j))
+    q.append((0, 0))
+    dist[0][0] = 1
     
     while q:
         x, y = q.popleft()
@@ -29,14 +20,8 @@ def bfs(i, j, maps, dist):
             nx = x + dx[k]
             ny = y + dy[k]
             
-            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+            if nx < 0 or nx >= len(maps) or ny < 0 or ny >= len(maps[0]):
                 continue
-            if dist[nx][ny] != INF:
-                continue
-            if maps[nx][ny] == 0:
-                continue
-                
-            dist[nx][ny] = dist[x][y] + 1
-            q.append((nx, ny))
-    
-    
+            if maps[nx][ny] == 1 and dist[nx][ny] == -1:
+                dist[nx][ny] = dist[x][y] + 1
+                q.append((nx, ny))
